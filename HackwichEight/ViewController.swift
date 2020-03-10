87579336d1c4d883187194e095eaddd48b8a6d0d
kat <kjacasio@hawaii.edu>
2020-03-10 10:43:01 -1000

    Hackwich 8 part 1-8 completed Mar 10

--- HackwichEight/ViewController.swift
+++ HackwichEight/ViewController.swift
@@ -10,11 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var slideLabel: UILabel!
+    @IBOutlet weak var onehundredLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var onehundred2Label: UILabel!
+    
+    var currentValue: Int = 0
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.numberLabel.text = "1"
+        self.onehundred2Label.text = "100"
+        self.slideLabel.text = "Slide as Close as you can to:"
+        self.onehundredLabel.text = "100"
     }
 
-
+    @IBAction func myGuessButtonPressed(_ sender: Any) {
+        
+        let alert = UIAlertController(title: "Hello World", message: "This is my first alert view" , preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        
+            alert.addAction(action)
+        
+            present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func slidermoved(_ sender: Any) {
+        
+        print("The value of the slider is:\(slider.value)")
+        
+        currentValue = lroundf(slider.value)
+        
+        
+    }
+    
+    
+    
+    
+    
+    
 }
 
